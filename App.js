a8a35c94b56cb5181bebcd003d46e09f8ede6e00
Kalidass <kalidass.k021@gmail.com>
2024-06-14 21:19:27 +0530

    foundation changes

--- App.js
+++ App.js
@@ -15,12 +15,28 @@ import ReactDOM from 'react-dom/client';
 // JSX - is not HTML in JS - It is HTML like syntax
 // JSX traspiled before it reaches the JS Engine - Parcel(Babel)
 // JSX => React.createElement => React Element (JS Object) => HTMLElement(while render)
-const jsxHeading = <h1 id='heading'>Hello, React using JSX</h1>;
 
-console.log('jsxHeading', jsxHeading);
+const elem = <span>React Element</span>
+const Title = () => (
+    <div>
+        {elem}
+        <h1 id='heading' className='head' tabIndex='5'>
+            Hello, React using JSX...
+        </h1>
+    </div>);
+
+// Component composition
+const HeadingComponent = () => (
+    <div className='container'>
+        {Title()}
+        <Title />
+        <Title></Title>
+        <h1>React functional component</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // root.render(heading);
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
